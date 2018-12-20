3c81ccaa7a3f75693bdb881d60fb0c3753661c8a
guilhermepachecod <guilhermepacheco@itwm.info>
2018-12-19 22:52:16 -0200

    first upload

--- installsamba.sh
+++ installsamba.sh
@@ -15,44 +15,24 @@
 # dpkg-reconfigure dash
 #remove apparmor e ferramentas
 
-sudo mkdir /media/hd2
-
 #só para servidor de testes
-sudo mkdir /media/hd2/Meus
-sudo mkdir /media/hd2/Meus/filmes
-sudo mkdir /media/hd2/Meus/pro
+sudo mkdir /media/minhapasta
+sudo mkdir /media/temp
 
 sudo apt-get install samba -y
 
-sudo wget http://10.0.0.102/files.tar
-sudo tar xvf files.tar
-sudo rm -f files.tar
+sudo wget https://github.com/guilhermepachecod/installsamba/smb.conf
 
 sudo service smbd stop
 
 sudo cp smb.conf /etc/samba/smb.conf
 
 #adiciona usuario para o samba
-echo "Escolha a senha para rede"
-sudo useradd rede -p rede -d /home/rede
-sudo smbpasswd -a rede
+echo "Escolha a senha para outrousuario"
+sudo useradd outrousuario -p outrousuario -d /home/outrousuario
+sudo smbpasswd -a outrousuario
 
-sudo rm -f httpd.conf
-sudo rm -f apache2.conf
-sudo rm -f php.ini
-sudo rm -f pptpd.conf
-sudo rm -f chap-secrets
-sudo rm -f proftpd.conf
-sudo rm -f shells
 sudo rm -f smb.conf
-sudo rm -f settings.json
-sudo rm -f my.cnf
-sudo rm -f star_tile2.gif
-sudo rm -f VelvetDrop2.otf
-sudo rm -f VelvetDrop.otf
-sudo rm -f index.html
-sudo rm -f fstab
-sudo rm -R /home/ftp
 
 sudo service smbd start
 
